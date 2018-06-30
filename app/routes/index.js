d51fe31df55cc12a14a237e09ef9b8d15d523814
Jonathan Spratley <jonniespratley@gmail.com>
2018-06-30 14:59:16 -0700

    updated files

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,8 @@ var redis = require('redis');
 var router = express.Router();
 
 /* redis */
-var host = process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.01';
-var port = process.env.REDIS_PORT_6379_TCP_PORT || 6379;
+var host = process.env.REDIS_HOST || '127.0.0.1';
+var port = process.env.REDIS_PORT || 6379;
 var client = redis.createClient(port, host);
 
 /* GET home page. */
